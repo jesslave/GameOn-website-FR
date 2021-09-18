57493655da938722dd0268159657e6ce0933c7a0
Jessica <jessicadeshpande10@gmail.com>
2021-09-18 15:43:25 +0200

    feature 3 : erreur formulaire + verfication avant validation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,7 +25,16 @@ const locationFiveInput = document.getElementById("location5");
 const locationSixInput = document.getElementById("location6");
 const checkboxOneInput = document.getElementById("checkbox1");
 const checkboxTwoInput = document.getElementById("checkbox2");
+//Errors
+const firstNameError = document.querySelector(".firstnameError");
+const nameError = document.querySelector(".nameError");
+const emailError = document.querySelector(".emailError");
+const birthdateError = document.querySelector(".birthdateError");
+const cityError = document.querySelector(".cityError");
+const conditionError = document.querySelector(".conditionError");
 
+const form = document.getElementById("form");
+form.addEventListener('submit', handleForm);
 
 
 // launch modal event
@@ -36,9 +45,20 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
-// Hide the background and the form on click on the X
+// Hide and Erase Data of the form
 function closeForm() {
   modalbg.style.display = "none"
+  eraseFormData();
+}
+
+// Hide the form
+function hideForm() {
+  modalbg.style.display = "none"
+}
+
+
+// Erase the datas
+function eraseFormData() {
   firstNameInput.value = "";
   lastNameInput.value = "";
   emailNameInput.value = "";
@@ -52,19 +72,64 @@ function closeForm() {
   locationFourInput.checked = false;
   locationFiveInput.checked = false;
   locationSixInput.checked = false;
-
 }
 
+// Hide the thanks modal then Erase the forms datas
 function closeFormValidationModal() {
   modalValidation.style.display = "none"
+  eraseFormData();
 }
 
-function validate(event) {
-  event.preventDefault();
-  modalValidation.style.display = "block";
+// Reset errors by putting normal border and none display for error labels
+function resetErrors() {
+  firstNameError.style.display = "none";
+  firstNameInput.style.borderColor = "transparent";
+  nameError.style.display = "none";
+  lastNameInput.style.borderColor = "transparent";
+  emailError.style.display = "none";
+  emailNameInput.style.borderColor = "transparent";
+  birthdateError.style.display = "none";
+  birthDateInput.style.borderColor = "transparent";
+  cityError.style.display = "none";
+  conditionError.style.display = "none";
 }
 
+function handleForm(event) { 
 
-function validate() {
-  modalValidation.style.display = "block";
-}
+  resetErrors();
+
+  if (firstNameInput.value.length < 2) {
+    firstNameError.style.display = "block";
+    firstNameInput.style.borderColor = "red";
+  }
+  else if (lastNameInput.value.length < 2) {
+    nameError.style.display = "block";
+    lastNameInput.style.borderColor = "red";
+  }
+  else if (emailNameInput.value.length < 2) {
+    emailError.style.display = "block";
+    emailNameInput.style.borderColor = "red";
+  }
+  else if (birthDateInput.value == '') {
+    birthdateError.style.display = "block";
+    birthDateInput.style.borderColor = "red";
+  }
+  else if (!checkboxOneInput.checked) {
+    conditionError.style.display = "block";
+  }
+  else if (quantityInput.value != '') {
+    if (!locationOneInput.checked && !locationTwoInput.checked && !locationThreeInput.checked && !locationFourInput.checked && !locationFiveInput.checked && !locationSixInput.checked) {
+      cityError.style.display = "block";
+    }
+    else {
+      hideForm(); 
+      modalValidation.style.display = "block"
+    }
+  }
+  else {
+    hideForm(); 
+    modalValidation.style.display = "block"
+   }
+  
+  event.preventDefault(); 
+} 
